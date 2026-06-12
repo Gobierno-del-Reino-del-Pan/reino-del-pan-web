3cc8bc20bb6238fbc7401cd7d6d52e8a4817caff
alexmartincontactooficial-hub <alexmartincontactooficial@gmail.com>
2026-06-12 18:01:40 +0200

    chore: remove unused vercel.json configuration file

--- vercel.js
+++
@@ -1,8 +0,0 @@
-
-{
-    "rewrites";[
-        { "source": "/auth/(.*)", "destination": "/auth/$1" },
-        { "source": "/api/(.*)", "destination": "/api/$1" },
-        { "source": "/(.*)", "destination": "/index.html" }
-    ]
-}
